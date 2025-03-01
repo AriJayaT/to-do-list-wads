be98cc2f7256a96fbcebe0804e14cd1888863559
Ari Jaya Teguh <ari.teguh@binus.ac.id>
2025-03-01 11:45:47 +0700

    profile page update

--- todo-list/src/Profile.jsx
+++ todo-list/src/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import profilePic from "./assets/pp.jpg";
 
 export default function Profile() {
   return (
@@ -7,15 +8,17 @@ export default function Profile() {
       <h1 className="text-3xl font-bold mb-4">Profile</h1>
       <div className="bg-purple-900 p-6 rounded-lg shadow-lg max-w-sm w-full">
         <div className="flex flex-col items-center">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Profile"
-            className="w-32 h-32 rounded-full mb-4"
-          />
           <h2 className="text-2xl font-bold">Ari Jaya Teguh</h2>
+          <img
+            src={profilePic}
+            alt="Profile Picture"
+            className="mt-4 w-32 h-32 rounded-full mb-4"
+          />
           <p className="mt-2 text-center">
-            A short bio goes here. I am passionate about building awesome web
-            applications and love to code!
+          My name is Ari Jaya Teguh, and I am a Computer Science student at Binus International University. 
+          I enjoy playing FPS games like Valorant and Counter-Strike, 
+          as well as watching anime, korean drama, and movies. My favorite anime is Tensei shitara Slime Datta Ken. 
+          I currently live in Alam Sutera.
           </p>
         </div>
       </div>
